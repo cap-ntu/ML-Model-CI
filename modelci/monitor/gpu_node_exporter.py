e4f2c17a050d99cc5addeed55c1857300a0c3607
HuaizhengZhang <huaizhen001@e.ntu.edu.sg>
2020-05-03 17:05:57 +0700

    [monitor] request info

--- modelci/monitor/gpu_node_exporter.py
+++ modelci/monitor/gpu_node_exporter.py
@@ -1,3 +1,25 @@
+import json
+import requests
+
+DEFAULT_GPU_MONITOR_PORT = "http://localhost:9400/gpu/metrics"
 
 
-DEFAULT_GPU_MONITOR_PORT = "localhost:9400/gpu/metrics"
+class GPUNodeExporter(object):
+
+    def __init__(self, service_url=None):
+        if service_url == None:
+            self.service_url = DEFAULT_GPU_MONITOR_PORT
+        else:
+            self.service_url = service_url
+
+    def requst_all_gpu_info(self):
+        info = requests.get(self.service_url)
+        print(info.json())
+
+
+
+
+
+if __name__ == '__main__':
+    a = GPUNodeExporter()
+    a.requst_all_gpu_info()
--- modelci/monitor/readme.md
+++ modelci/monitor/readme.md
@@ -8,4 +8,10 @@ start the nvidia node exporter
 sh start_node_exporter.sh
 ```
 
+check the information
+
+```bash
+curl -s localhost:9400/gpu/metrics
+```
+
 ## cAdvisor for monitoring the other resource usage
