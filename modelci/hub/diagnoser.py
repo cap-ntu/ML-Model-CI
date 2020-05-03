059291156444ed02247f635a0c61cbbad0be8c93
Yizheng Huang <huangyz0918@gmail.com>
2020-05-03 15:14:39 +0800

    add diagnoser

--- modelci/hub/diagnoser.py
+++ modelci/hub/diagnoser.py
@@ -1,1 +1,28 @@
-# Profile models
+"""
+Author: huangyz0918
+Dec: prolifing models.
+Date: 03/05/2020
+"""
+
+from modelci.metrics.benchmark.metric import BaseDataWrapper, BaseModelInspector
+
+class Diagnoser(object):
+    """
+    Diagnoser class, call this to test model performance.
+    """
+
+    def __init__(self, inspector:BaseModelInspector, server_name):
+        """
+        Init a diagnoser object
+
+        @param inspector:BaseModelInspector
+        """
+        self.inspector = inspector
+        self.server_name
+
+    def diagnose(self):
+        """
+        start diagnosing and profiling model.
+
+        """
+        inspector.run_model(server_name) 
