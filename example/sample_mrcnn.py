fd0317db91b0bda45fcdce8f6507e2445f3ff789
huangyz0918 <huangyz0918@gmail.com>
2020-06-07 01:02:45 +0800

    🚑 quickfix:

--- example/sample_mrcnn.py
+++ example/sample_mrcnn.py
@@ -8,13 +8,13 @@ from modelci.hub.profiler import Profiler
 from modelci.persistence.bo.model_objects import Engine, Framework
 
 if __name__ == "__main__":
-    data_path = '' # test data path
+    data_path = '' # test image data path
     with open(data_path, 'rb') as f: # for TensorFlow Serving
-        file_path = '' # Mask R-CNN model config file saved path
+        file_path = '' # Mask R-CNN model config file path
         tfs_client = CVTFSClient(f.read(), batch_num=100, batch_size=32, asynchronous=True)
 
         register_model_from_yaml(file_path) # register your model in the database
-        mode_info = retrieve_model_by_name(architecture_name='ResNet50', framework=Framework.PYTORCH,engine=Engine.TORCHSCRIPT) # retrieve modelm information
+        mode_info = retrieve_model_by_name(architecture_name='ResNet50', framework=Framework.PYTORCH,engine=Engine.TORCHSCRIPT) # retrieve model information
 
         profiler = Profiler(model_info=mode_info, server_name='tfs', inspector=tfs_client)
-        profiler.auto_diagnose([2, 4, 16]) # profile batch size 2, 4, 6 inb all availavle devices
+        profiler.auto_diagnose([2, 4, 16]) # profile batch size 2, 4, 6 in all the availavle devices
