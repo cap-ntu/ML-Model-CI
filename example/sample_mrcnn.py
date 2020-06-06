45688821296a54ff0be484ccc34518962fd5d893
huangyz0918 <huangyz0918@gmail.com>
2020-06-07 01:00:36 +0800

    🚑 quickfix:

--- example/sample_mrcnn.py
+++ example/sample_mrcnn.py
@@ -10,10 +10,10 @@ from modelci.persistence.bo.model_objects import Engine, Framework
 if __name__ == "__main__":
     data_path = '' # test data path
     with open(data_path, 'rb') as f: # for TensorFlow Serving
-        model_path = '' # Mask R-CNN model saved path
+        file_path = '' # Mask R-CNN model config file saved path
         tfs_client = CVTFSClient(f.read(), batch_num=100, batch_size=32, asynchronous=True)
 
-        register_model_from_yaml(model_path) # register your model in the database
+        register_model_from_yaml(file_path) # register your model in the database
         mode_info = retrieve_model_by_name(architecture_name='ResNet50', framework=Framework.PYTORCH,engine=Engine.TORCHSCRIPT) # retrieve modelm information
 
         profiler = Profiler(model_info=mode_info, server_name='tfs', inspector=tfs_client)
