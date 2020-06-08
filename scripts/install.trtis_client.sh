2c7453327960044015a7fab2536cdabc5322aebd
YuanmingLeee <YLI056@e.ntu.edu.sg>
2020-06-08 23:47:34 +0800

    [quick fix] :scroll: Fix install.trtis_client.sh

--- scripts/install.trtis_client.sh
+++ scripts/install.trtis_client.sh
@@ -4,6 +4,11 @@ VERSION=1.8.0
 UBUNTU_VERSION=$(lsb_release -sr | tr -d '.')
 FILENAME=v"${VERSION}"_ubuntu"${UBUNTU_VERSION}".clients.tar.gz
 
+function download_file_and_untar() {
+    wget https://github.com/NVIDIA/tensorrt-inference-server/releases/download/v"${VERSION}"/"${FILENAME}"
+    tar xzf "${FILENAME}"
+}
+
 mkdir -p ~/tmp
 cd ~/tmp || return 1
 mkdir -p tensorrtserver && cd tensorrtserver || return 1
@@ -11,10 +16,9 @@ mkdir -p tensorrtserver && cd tensorrtserver || return 1
 # get package
 if [ -f "${FILENAME}" ] ; then
   echo "Already downloaded at ${FILENAME}"
-  tar xzf "${FILENAME}"
+  tar xzf "${FILENAME}" || download_file_and_untar
 else
-  wget https://github.com/NVIDIA/tensorrt-inference-server/releases/download/v"${VERSION}"/"${FILENAME}"
-  tar xzf "${FILENAME}"
+    download_file_and_untar
 fi
 
 # install
