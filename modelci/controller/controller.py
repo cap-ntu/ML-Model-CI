6826c675bcc95fda0cb9ebc9c0100ec9a38307e1
HuaizhengZhang <huaizhen001@e.ntu.edu.sg>
2020-06-16 15:22:38 +0700

    [misc] fix codacy

--- modelci/controller/controller.py
+++ modelci/controller/controller.py
@@ -1,17 +1,16 @@
 from modelci.hub.profiler import Profiler
 from modelci.monitor.gpu_node_exporter import GPUNodeExporter
 
-def profiler_callback(model_info, Profiler, device_util_thd=0, device_memory_thd=0, period=60):
+def profiler_callback(model_info, profiler=Profiler, device_util_thd=0, device_memory_thd=0, period=60):
 
     # After conversion, we will get a model object that contains mode info
     # use the model info to init a client and a profiler
-    profiler = Profiler(model_info=mode_info)
+    profiler = profiler(model_info=model_info)
 
     # Here we assume that each worker server only contains one kind of GPU
 
-
     # Use the monitor to get devices names (https://github.com/anderskm/gputil), IDs and utilization periodically.
-
+    gpu_collector = GPUNodeExporter()
 
     # if the utilization of any of them is lower than device_util_thd=0, device_memory_thd=0
     # 0）stop the period function
@@ -21,7 +20,7 @@ def profiler_callback(model_info, Profiler, device_util_thd=0, device_memory_thd
     profiler.auto_diagnose()
 
 def auto_device_placement():
-    raise NotImplementedError
+    raise NotImplementedError('Method `auto_device_placement` is not implemented.')
 
 
    
