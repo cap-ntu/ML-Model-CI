0fbd4679e03caeb4d650f0c04e7267321e5a0b4b
YuanmingLeee <YLI056@e.ntu.edu.sg>
2020-09-04 12:02:04 +0800

    [fix] ONNX GPU serving startup script

--- modelci/hub/deployer/onnxs/deploy_model_gpu.sh
+++ modelci/hub/deployer/onnxs/deploy_model_gpu.sh
@@ -2,5 +2,5 @@
 
 docker run -d --rm -p "${2}":8000 -p "${3}":8001 \
   --runtime=nvidia \
-  --mount type=bind,source="${HOME}"/.modelci/"${1}"/pytorch-onnx,target=/model/"${1}" \
+  --mount type=bind,source="${HOME}"/.modelci/"${1}"/pytorch-onnx,target=/models/"${1}" \
   -e MODEL_NAME="${1}" --env-file docker-env.env -t mlmodelci/onnx-serving:latest-gpu
