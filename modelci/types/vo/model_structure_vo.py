16019426e57320f43e2a232250712b82a024d1e5
YuanmingLeee <YLI056@e.ntu.edu.sg>
2021-01-29 00:19:03 +0800

    :wrench: Fix ML Model structure model
    * Remove uncleared private attribute
    * Add parse from pytorch nn module function
    * Simplify type hint for `ModelLayer`

--- modelci/types/vo/model_structure_vo.py
+++ modelci/types/vo/model_structure_vo.py
@@ -6,6 +6,7 @@ Email: yli056@e.ntu.edu.sg
 Date: 1/27/2021
 """
 import abc
+import inspect
 from enum import Enum
 from typing import Optional, Union, Tuple, Dict, OrderedDict
 
@@ -16,12 +17,13 @@ from typing_extensions import Literal
 class Operation(Enum):
     """
     Operation enum to the layer or connection. There are three kinds of operations: ``'A'`` for add the specific
-    layer / connection, ``'D'`` for delete the specific layer / connection, and ``M`` for modify the layer /
-    connection.
+    layer / connection, ``'D'`` for delete the specific layer / connection, ``M`` for modify the layer /
+    connection, and ``E`` for no operation.
     """
     ADD = 'A'
     DELETE = 'D'
     MODIFY = 'M'
+    EMPTY = 'E'
 
 
 class LayerType(Enum):
@@ -29,13 +31,13 @@ class LayerType(Enum):
     Enum of the supported layer type. This is to hint which class of layer the provided data is converted to.
     """
 
-    LINEAR = 'nn.Linear'
-    CONV_1D = 'nn.Conv1d'
-    CONV_2D = 'nn.Conv2d'
-    RELU = 'nn.ReLU'
-    TANH = 'nn.Tanh'
-    BN_1D = 'nn.BatchNorm1d'
-    BN_2D = 'nn.BatchNorm2d'
+    LINEAR = 'torch.nn.Linear'
+    CONV_1D = 'torch.nn.Conv1d'
+    CONV_2D = 'torch.nn.Conv2d'
+    RELU = 'torch.nn.ReLU'
+    TANH = 'torch.nn.Tanh'
+    BN_1D = 'torch.nn.BatchNorm1d'
+    BN_2D = 'torch.nn.BatchNorm2d'
 
 
 class ModelLayer(BaseModel, abc.ABC):
@@ -43,31 +45,63 @@ class ModelLayer(BaseModel, abc.ABC):
     """
     Layer of the model structure.
 
+    For layer attributes need to be set :code:`None`, use :code:`'null'` instead. This is for the reason of
+    updated parameters with value :code:`None` will be viewed as not set. So we take special care to the
+    desired :code:`None`, replacing it with :code:`'null'`.
+
     Attributes:
-        _op (Operation): Operation to the layer.
-        layer_type (LayerType): Indicates the type of this layer. This field also provides hint for :class:`pydantic`
+        op_ (Operation): Operation to the layer.
+        type_ (LayerType): Indicates the type of this layer. This field also provides hint for :class:`pydantic`
             model conversion.
+        __required_type__ (LayerType): By overriding this attributes, we can use :meth:`check_layer_type` to
+            provide validation of the sub classes.
     """
 
-    _op: Operation
-    layer_type: LayerType
+    op_: Operation
+    type_: LayerType
 
-    class Config:
-        fields = {'layer_type': '_type'}
+    __required_type__: LayerType
 
+    @classmethod
+    def parse_layer_obj(cls, layer_obj):
+        """
+        Parse from a ML layer object.
 
-def check_layer_type_factory(required_value: LayerType):
-    """
-    Factory that checks layer type value provided is the same as the required value.
-    This is to generate validator for check :code:`layer_type` field of subclasses of :class:`ModelLayer`.
-    """
+        This function will inspect the required parameters to build the layer, and try to obtain its
+        parameter value from the layer object. The default parameter parser is python default
+        :code:`getattr`, which assume we can get the value from the same-named attribute of the
+        layer object.
 
-    def check_layer_type(layer_type: LayerType) -> LayerType:
-        if layer_type != required_value:
-            raise ValueError(f'Expected {required_value} but got {layer_type}')
+        For parameter cannot parsed with default parser, set a function with the format:
+        :code:`__{parameter_name}_parser__(layer_obj: Any) -> Any`.
+        Has the following signature:
+            Input Arguments:
+            * layer_obj : Any
+                The layer object to be parsed.
+            Return Arguments:
+            * Any
+                The parsed value of the given parameter.
+
+        TODO:
+            Signature checking for __{parameter_name}_parser__
+        """
+        kwargs = {'op_': Operation.EMPTY, 'type_': cls.__required_type__}
+        signature = inspect.signature(layer_obj.__init__)
+        for param in signature.parameters:
+            parser = getattr(cls, f'__{param}_parser__', lambda obj: getattr(obj, param))
+            kwargs[param] = parser(layer_obj)
+
+        return cls(**kwargs)
+
+    @validator('type_')
+    def check_layer_type(cls, layer_type: LayerType) -> LayerType:  # noqa
+        """
+        Checks layer type value provided is the same as the required value.
+        This is to generate validator for check :code:`layer_type` field of subclasses of :class:`ModelLayer`.
+        """
+        if layer_type != cls.__required_type__:
+            raise ValueError(f'Expected {cls.__required_type__} but got {layer_type}')
         return layer_type
-
-    return check_layer_type
 
 
 class LinearLayer(ModelLayer):
@@ -75,7 +109,11 @@ class LinearLayer(ModelLayer):
     out_features: Optional[PositiveInt]
     bias: Optional[bool]
 
-    _check_type = validator('layer_type', allow_reuse=True)(check_layer_type_factory(LayerType.LINEAR))
+    __required_type__ = LayerType.LINEAR
+
+    @staticmethod
+    def __bias_parser__(layer_obj):
+        return layer_obj.bias is not None
 
 
 class _ConvNd(ModelLayer, abc.ABC):
@@ -89,29 +127,33 @@ class _ConvNd(ModelLayer, abc.ABC):
     bias: bool
     padding_mode: Literal['zeros', 'reflect', 'replicate', 'circular']
 
+    @staticmethod
+    def __bias_parser__(layer_obj):
+        return layer_obj.bias is not None
+
 
 class Conv1d(_ConvNd):
     kernel_size: Optional[Union[PositiveInt, Tuple[PositiveInt]]]
     stride: Optional[Union[PositiveInt, Tuple[PositiveInt]]]
 
-    _check_type = validator('layer_type', allow_reuse=True)(check_layer_type_factory(LayerType.CONV_1D))
+    __required_type__ = LayerType.CONV_1D
 
 
 class Conv2d(_ConvNd):
     kernel_size: Optional[Union[PositiveInt, Tuple[PositiveInt, PositiveInt]]]
     stride: Optional[Union[PositiveInt, Tuple[PositiveInt, PositiveInt]]]
 
-    _check_type = validator('layer_type', allow_reuse=True)(check_layer_type_factory(LayerType.CONV_2D))
+    __required_type__ = LayerType.CONV_2D
 
 
 class ReLU(ModelLayer):
     inplace: Optional[bool]
 
-    _check_type = validator('layer_type', allow_reuse=True)(check_layer_type_factory(LayerType.RELU))
+    __required_type__ = LayerType.RELU
 
 
 class Tanh(ModelLayer):
-    _check_type = validator('layer_type', allow_reuse=True)(check_layer_type_factory(LayerType.TANH))
+    __required_type__ = LayerType.TANH
 
 
 class _BatchNorm(ModelLayer, abc.ABC):
@@ -123,11 +165,11 @@ class _BatchNorm(ModelLayer, abc.ABC):
 
 
 class BatchNorm1d(_BatchNorm):
-    _check_type = validator('layer_type', allow_reuse=True)(check_layer_type_factory(LayerType.BN_1D))
+    __required_type__ = LayerType.BN_1D
 
 
 class BatchNorm2d(_BatchNorm):
-    _check_type = validator('layer_type', allow_reuse=True)(check_layer_type_factory(LayerType.BN_2D))
+    __required_type__ = LayerType.BN_2D
 
 
 _LayerType = Union[LinearLayer, Conv1d, Conv2d, ReLU, Tanh, BatchNorm1d, BatchNorm2d]
@@ -151,7 +193,7 @@ class Structure(BaseModel):
         >>> from collections import OrderedDict
         >>> # add a nn.Linear layer named 'fc1' with in_features=1024, out_features=10
         >>> layer_mapping = OrderedDict({
-        ...     'fc1': LinearLayer(in_features=1024, out_features=10, _type=LayerType.LINEAR, _op=Operation.ADD),
+        ...     'fc1': LinearLayer(in_features=1024, out_features=10, type_=LayerType.LINEAR, op_=Operation.ADD),
         ... })
         >>> # connection example for add connection from 'conv1' to 'fc1'
         >>> connection_mapping = {'conv1': {'fc1': Operation.ADD}}
@@ -162,7 +204,7 @@ class Structure(BaseModel):
         >>> # Other than using the model object, we can pass in a plain dictionary,
         ... # and utilize `Structure.parse_obj`.
         >>> structure_data = {
-        ...     'layer': {'fc': {'in_features': 1024, 'out_features': 10, '_type': 'nn.Linear', '_op': 'A'}},
+        ...     'layer': {'fc': {'in_features': 1024, 'out_features': 10, 'type_': 'torch.nn.Linear', 'op_': 'A'}},
         ...     'connection': {'conv1': {'fc1': 'A'}}
         ... }
         >>> Structure.parse_obj(structure_data)
@@ -172,7 +214,7 @@ class Structure(BaseModel):
     """
     layer: OrderedDict[str, _LayerType] = Field(
         default_factory=OrderedDict,
-        example={'fc': {'out_features': 10, '_type': 'nn.Linear', '_op': 'M'}}
+        example={'fc': {'out_features': 10, 'type_': 'torch.nn.Linear', 'op_': 'M'}}
     )
     connection: Optional[Dict[str, Dict[str, Operation]]] = Field(
         default_factory=dict,
