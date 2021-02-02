c4811f861a316b020b2e8898c79a8db95710132a
Huaizheng Zhang <huaizhen001@e.ntu.edu.sg>
2021-02-02 23:04:11 +0800

    [cli] start to improve cli

---
+++ modelci/cli/model_manager.py
@@ -0,0 +1,15 @@
+import cli
+
+
+
+def register():
+    raise NotImplementedError
+
+def show_models():
+    raise NotImplementedError
+
+def download_model():
+    raise NotImplementedError
+
+def download_model_from_url():
+    raise NotImplementedError
---
+++ modelci/cli/modelci_service.py
@@ -0,0 +1,23 @@
+import cli
+
+
+def start_leader_server():
+    raise NotImplementedError
+
+
+def stop_leader_server():
+    raise NotImplementedError
+
+
+
+def connect_leader_server():
+    raise NotImplementedError
+
+
+def disconnect_leader_server():
+    raise NotImplementedError
+
+
+
+
+
