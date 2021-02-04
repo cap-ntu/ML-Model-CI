ec87ef0a41347691baf26f9458eadb945bfb1030
Huaizheng Zhang <huaizhen001@e.ntu.edu.sg>
2021-02-04 19:49:46 +0800

    [cli] add model downloader

---
+++ modelci/hub/publish.py
@@ -0,0 +1,77 @@
+#  Copyright (c) NTU_CAP 2021. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+
+import os
+import tempfile
+import hashlib
+import shutil
+
+
+from urllib.request import urlopen, Request
+from urllib.parse import urlparse
+from tqdm.auto import tqdm
+
+
+
+# TODO: move model registeration related functions from manager to this file
+
+def download_model_from_url(url, file_path, hash_prefix=None, progress=True):
+    file_size = None
+    
+    req = Request(url)
+    u = urlopen(req)
+    meta = u.info()
+    if hasattr(meta, 'getheaders'):
+        content_length = meta.getheaders("Content-Length")
+    else:
+        content_length = meta.get_all("Content-Length")
+    if content_length is not None and len(content_length) > 0:
+        file_size = int(content_length[0])
+    print(file_size)
+
+    dst = os.path.expanduser(file_path)
+    dst_dir = os.path.dirname(dst)
+    f = tempfile.NamedTemporaryFile(delete=False, dir=dst_dir)
+
+    try:
+        if hash_prefix is not None:
+            sha256 = hashlib.sha256()
+        with tqdm(total=file_size, disable=not progress,
+                unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+            while True:
+                buffer = u.read(8192)
+                if len(buffer) == 0:
+                    break
+                f.write(buffer)
+                if hash_prefix is not None:
+                    sha256.update(buffer)
+                pbar.update(len(buffer))
+
+        f.close()
+        if hash_prefix is not None:
+            digest = sha256.hexdigest()
+            if digest[:len(hash_prefix)] != hash_prefix:
+                raise RuntimeError('invalid hash value (expected "{}", got "{}")'
+                                .format(hash_prefix, digest))
+        shutil.move(f.name, dst)
+    finally:
+        f.close()
+        if os.path.exists(f.name):
+            os.remove(f.name)
+
+
+if __name__ == "__main__":
+    resnet18_url = "https://s3.amazonaws.com/pytorch/models/resnet18-5c106cde.pth"
+    model_name = "resnet18.pth"
+    download_model_from_url(resnet18_url, model_name)
