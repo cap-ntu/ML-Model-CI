bf3138e407d314179d89fb5fea7cc08b29af92b0
univerone <univerone@outlook.com>
2021-02-07 21:49:47 +0800

    💄 Add job table

--- frontend/src/layouts/BasicLayout/menuConfig.ts
+++ frontend/src/layouts/BasicLayout/menuConfig.ts
@@ -1,26 +1,11 @@
 const headerMenuConfig = [];
 
 const asideMenuConfig = [
-  {
-    name: 'Dashboard',
-    path: '/',
-    icon: 'chart-pie',
-  },
-  {
-    name: 'Users',
-    path: '/user',
-    icon: 'account',
-  },
-  {
-    name: 'Settings',
-    path: '/setting',
-    icon: 'set',
-  },
-  {
-    name: 'About',
-    path: '/about',
-    icon: 'warning',
-  },
+  { name: 'Dashboard', path: '/', icon: 'chart-pie' },
+  { name: 'Users', path: '/user', icon: 'account' },
+  { name: 'Settings', path: '/setting', icon: 'set' },
+  { name: 'About', path: '/about', icon: 'warning' },
+  { name: 'Jobs', path: '/jobs', icon: 'edit' },
 ];
 
 export { headerMenuConfig, asideMenuConfig };
--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -4,34 +4,21 @@ import About from '@/pages/About';
 import Setting from '@/pages/Setting';
 import User from '@/pages/User';
 import Visualizer from '@/pages/Visualizer';
+import Jobs from '@/pages/Jobs';
 
 const routerConfig = [
   {
     path: '/',
     component: BasicLayout,
     children: [
-      {
-        path: '/',
-        exact: true,
-        component: Dashboard,
-      },
-      {
-        path: '/about',
-        component: About,
-      },
-      {
-        path: '/user',
-        component: User,
-      },
-      {
-        path: '/setting',
-        component: Setting,
-      },
-      {
-        path: '/visualizer/:id',
-        component: Visualizer,
-      },
+      { path: '/about', component: About },
+      { path: '/user', component: User },
+      { path: '/setting', component: Setting },
+      { path: '/visualizer/:id', component: Visualizer },
+      { path: '/', exact: true, component: Dashboard },
+      { path: '/jobs', component: Jobs },
     ],
   },
 ];
+
 export default routerConfig;
