6f94f13a56820f58cc47ceb86414776ddb11b955
YuanmingLeee <YLI056@e.ntu.edu.sg>
2021-03-03 01:05:31 +0800

    :memo: Update pydoc for API publish model

--- modelci/app/v1/endpoints/model.py
+++ modelci/app/v1/endpoints/model.py
@@ -49,18 +49,48 @@ async def publish_model(
         files: List[UploadFile] = File(
             [],
             description='This field can be set with empty value. In such settings, the publish is a dry run to'
-                        'validate the `ml_model_in_form` field. You are recommend to try a dry run before the'
-                        'model wight file(s) are posted. This is because the weight file(s) are usually large and '
-                        'take a while to receive the whole request and validate.'
+                        'validate the `ml_model_in_form` field. You are recommend to try a dry run to find input'
+                        'errors before you send the wight file(s) to the server.'
         ),
         convert: bool = True,
         profile: bool = False
 ):
+    """Publish model to the model hub. The model weight file(s) as well as its meta information (e.g.
+    architecture, framework, and serving engine) will be stored into the model hub.
+
+    The publish API will also automatically convert the published model into other deployable format such as
+    TorchScript and ONNX. After successfully converted, original model and its generated models will be profiled
+    on the underlying devices in the clusters, and collects, aggregates, and processes running model performance.
+
+    Args:
+        ml_model_in_form (MLModelInForm): Model meta information in the form of `multipart/formdata`.
+        files (List[UploadFile]): A list of model weight files. The files are organized accordingly. Their file name
+            contains relative path to their common parent directory.
+            If the files is empty value, a dry-run to this API is conducted for parameter checks. No information
+            will be saved into model hub in this case.
+        convert (bool): Flag for auto configuration.
+        profile (bool): Flag for auto profiling.
+
+    Returns:
+        A message response, with IDs of all published model. The format of the return is:
+        ```
+        {
+          "data": {"id": ["603e6a1f5a62b08bc0a2a7f2", "603e6a383b00cbe9bfee7277"]},
+          "status": true
+        }
+        ```
+        Specially, if the dry-run test passed, it will return a status True:
+        ```
+        {
+          "status": true
+        }
+        ```
+    """
     # save the posted files as local cache
     loop = asyncio.get_event_loop()
     saved_path = ml_model_in_form.saved_path
     if len(files) == 0:
-        # dry run
+        # conduct dry run for parameter check only.
         return {'status': True}
     if len(files) == 1:
         file = files[0]
